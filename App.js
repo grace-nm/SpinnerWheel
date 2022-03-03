535517f83e4e82d9572d124db156c16178edf756
grace-nm <grace.mcloughlin@ezyvet.com>
2022-03-04 09:26:48 +1300

    adding

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SpinWheel from './SpinWheel';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Who is leading scrum today?...</Text>
       <StatusBar style="auto" />
+      <SpinWheel />
     </View>
   );
 }
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 22,
+    color: 'white',
+    marginTop: 40,
+  },
 });
+
+
